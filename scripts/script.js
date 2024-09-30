9deffda6d754366eead31948997595b67a5a8ddf
Rémi Collet <165579109+RCollet97@users.noreply.github.com>
2024-09-30 11:01:35 +0200

    Delete my helper

--- scripts/script.js
+++ scripts/script.js
@@ -18,19 +18,16 @@ function calculateResult() {
     try {
         const expression = display.value.replace(/,/g, "."); 
         
-        // Gérer les pourcentages en transformant "X%" en "X / 100"
         const percentageExpression = expression.replace(/(\d+(\.\d+)?)(%)/g, "($1 / 100)");
         
-        // Évalue l'expression modifiée
         let result = eval(percentageExpression);
         
-        // Vérifie si le résultat est un nombre et remplace le point par une virgule
         if (!isNaN(result)) {
-            display.value = result.toString().replace(".", ","); // Remplace le point par une virgule
+            display.value = result.toString().replace(".", ",");
         } else {
-            display.value = "Erreur"; // Affiche une erreur si le résultat n'est pas un nombre
+            display.value = "Erreur";
         }
     } catch (error) {
-        display.value = "Erreur"; // Affiche une erreur si l'évaluation échoue
+        display.value = "Erreur";
     }
 }
